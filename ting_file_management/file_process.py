240ac501f56a99e138b8e17ae8be768a269ff9c0
jandui <janduineto18@gmail.com>
2023-12-06 21:38:43 -0300

    feat: add metod remove in process and metode for seach file

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,10 +1,35 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    text_data = txt_importer(path_file)
+    for index in range(len(instance)):
+        data_file = instance.search(index)
+        if data_file['nome_do_arquivo'] == path_file:
+            return
+    data = dict()
+    data['nome_do_arquivo'] = path_file
+    data['qtd_linhas'] = len(text_data)
+    data["linhas_do_arquivo"] = text_data
+    instance.enqueue(data)
+    print(data, file=sys.stdout)
+
+    return data
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if len(instance) == 0:
+        return print("Não há elementos", file=sys.stdout)
+    data_remove = instance.dequeue()
+    path_file = data_remove['nome_do_arquivo']
+    print(f"Arquivo {path_file} removido com sucesso", file=sys.stdout)
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    try:
+        data = instance.search(position)
+        print(data, file=sys.stdout)
+        return data
+    except IndexError:
+        print("Posição inválida", file=sys.stderr)
