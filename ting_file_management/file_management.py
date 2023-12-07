81a70d4e77e389680ee328f83e5ff377d6a64a2b
jandui <janduineto18@gmail.com>
2023-12-06 21:01:50 -0300

    feat: add method to read a txt file

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,15 @@
-def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+import sys
+
+
+def txt_importer(path_file: str):
+    try:
+        if not path_file.endswith(".txt"):
+            print("Formato inválido", file=sys.stderr)
+
+        file = open(path_file, 'r')
+        content = file.read()
+        list_words = content.split('\n')
+
+        return list_words
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
