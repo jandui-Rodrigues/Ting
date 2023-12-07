3b255846eece6393f43eb67bc87e317433ca5469
jandui <janduineto18@gmail.com>
2023-12-06 22:39:45 -0300

    feat: created function that detects if a certain word exists in a file

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,24 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result_search = list()
+
+    for index in range(len(instance)):
+        search_word_file = dict()
+        data_file = instance.search(index)
+        phares = data_file['linhas_do_arquivo']
+        ocurrences = list()
+        index = 1
+        for phare in phares:
+            if word in phare.lower():
+                ocurrences.append({"linha": index})
+                search_word_file["palavra"] = word
+                search_word_file["arquivo"] = data_file['nome_do_arquivo']
+            index += 1
+        print(ocurrences)
+        if len(ocurrences) != 0:
+            search_word_file["ocorrencias"] = ocurrences
+            result_search.append(search_word_file)
+
+    return result_search
 
 
 def search_by_word(word, instance):
