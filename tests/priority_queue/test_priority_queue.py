52544b2c1231d9d3bd585022df076cd9beb2eab4
jandui <janduineto18@gmail.com>
2023-12-07 12:37:55 -0300

    test: Tested for the PriorityQueue class

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,23 @@
-# from ting_file_management.priority_queue import PriorityQueue
+from ting_file_management.priority_queue import PriorityQueue
+import pytest
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    priority_queue = PriorityQueue()
+
+    assert len(priority_queue) == 0
+
+    priority_queue.enqueue({"qtd_linhas": 4})
+    priority_queue.enqueue({"qtd_linhas": 1})
+    priority_queue.enqueue({"qtd_linhas": 7})
+    priority_queue.enqueue({"qtd_linhas": 3})
+
+    assert len(priority_queue) == 4
+    assert priority_queue.dequeue() == {"qtd_linhas": 4}
+    assert priority_queue.dequeue() == {"qtd_linhas": 1}
+    assert len(priority_queue) == 2
+
+    assert priority_queue.search(0) == {"qtd_linhas": 3}
+
+    with pytest.raises(IndexError):
+        priority_queue.search(99)
